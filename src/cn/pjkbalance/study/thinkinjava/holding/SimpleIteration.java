7a6f42ba8ad5ca179ee47270cdd5a9255824c7ce
jiakang <pengjiakang@champion-ic.com>
2018-06-12 19:02:55 +0800

    2018061201 think in java

---
+++ src/cn/pjkbalance/study/thinkinjava/holding/SimpleIteration.java
@@ -0,0 +1,35 @@
+package cn.pjkbalance.study.thinkinjava.holding;
+
+import cn.pjkbalance.common.util.Print;
+import typeinfo.pets.Pet;
+import typeinfo.pets.Pets;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by jiakang on 2018/6/7.
+ */
+public class SimpleIteration {
+    public static void main(String... args) {
+        List<Pet> pets = Pets.arrayList(12);
+        Iterator<Pet> it = pets.iterator();
+        while(it.hasNext()) {
+            Pet pet = it.next();
+            System.out.print(pet.id() + "="+pet+" ");
+        }
+        Print.print();
+        for(Pet pet : pets) {
+            System.out.print(pet.id() + "="+pet+" ");
+        }
+        Print.print();
+        it = pets.iterator();
+        for (int i = 0; i < 6; i++) {
+            Pet pet = it.next();
+            System.out.print(pet.id() + "="+pet+" ");
+            it.remove();
+        }
+        Print.print();
+        Print.print(pets);
+    }
+}
