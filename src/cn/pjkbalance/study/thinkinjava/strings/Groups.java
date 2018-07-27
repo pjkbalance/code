75d6d139973d2e1d63915f4f4b81f7257cd7721b
jiakang <pengjiakang@champion-ic.com>
2018-07-27 12:38:07 +0800

    think in java 20180727

---
+++ src/cn/pjkbalance/study/thinkinjava/strings/Groups.java
@@ -0,0 +1,24 @@
+package cn.pjkbalance.study.thinkinjava.strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jiakang on 2018/7/26.
+ */
+public class Groups {
+    static public final String POEM = "j.k peng is a good man, \n" +
+            "j.k peng is a bad man; \n" +
+            "j.k is too bad to be killed.\n";
+    private static Pattern p = Pattern.compile("(?m)((\\S+)(\\s+))$");
+    private static Pattern p1 = Pattern.compile("(\\S+)((\\s+)$)");
+    public static void main(String... args) {
+        Matcher m = p.matcher(POEM);
+        while (m.find()) {
+            for (int j = 0; j <= m.groupCount(); j++) {
+                System.out.print("["+j+"-"+m.group(j)+"]");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/cn/pjkbalance/study/thinkinjava/strings/StartEnd.java
@@ -0,0 +1,17 @@
+package cn.pjkbalance.study.thinkinjava.strings;
+
+/**
+ * Created by jiakang on 2018/7/27.
+ */
+public class StartEnd {
+    public static String input = "As long as there is injustice, whenever a\n" +
+            "Targathian baby cries out, wherever a distress\n" +
+            "Signal sounds among the stars ... We'll be there.\n" +
+            "This fine ship, and this fine crew ...\n" +
+            "Never give up! Never surrender!";
+    private static class Display {
+        private boolean regexPrinted = false;
+        private String regex;
+
+    }
+}
